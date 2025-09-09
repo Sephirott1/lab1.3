ef103214a41f68ebb40d6c68e8159069d1c21805
Sephirott1 <149088259+Sephirott1@users.noreply.github.com>
2025-09-09 22:22:19 +0300

    Add files via upload

---
+++ Lab1/Lab1_Task3/Lab1_Task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "RUS ");
+
+	puts("      _  _   _  _");
+	puts(" | |.| |  |.| |  |");
+	puts(" | |.| |  |.| |  |");
+	puts("      -      -    ");
+}
---
+++ Lab1/laba1/Lab1_Task1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "RUS ");
+
+	puts("§¯§Ñ§Ø§Þ§Ú§ä§Ö Enter §Õ§Ý§ñ §á§â§à§Õ§à§Ý§Ø§Ö§ß§Ú§ñ...");
+	getchar();
+	puts("§±§â§à§Õ§à§Ý§Ø§Ö§ß§Ú§Ö §á§â§à§Ô§â§Ñ§Þ§Þ§í");
+
+	return 0;
+}
