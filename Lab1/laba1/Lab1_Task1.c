22ab96933a7daf2e7f5073e313280fa03e22498a
Sephirott1 <149088259+Sephirott1@users.noreply.github.com>
2025-09-09 22:31:05 +0300

    Add files via upload

---
+++ Lab1/laba1/Lab1_Task1.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "RUS ");
+
+	puts("Нажмите Enter для продолжения...");
+	getchar();
+	puts("Продолжение программы");
+
+	return 0;
+}
